32545395ab9b7a44dc162d8bf7b680297c0c626e
Gustavo Zambonin <stenny_gus@hotmail.com>
2015-05-09 20:35:24 -0300

    some small changes

---
+++ README.md
@@ -0,0 +1,8 @@
+This will open any number of files given through arguments and print the sorted distribution of letter and digram frequencies. It's not guaranteed to be fast, though.
+
+```
+chmod u+x main.py
+./main.py foo.in bar.in
+```
+
+A free copy of Victor Hugo's _Les Miserábles_ in TXT format is included for testing purposes.
--- main.py
+++ main.py
@@ -8,39 +8,30 @@ allowed_letters = string.ascii_lowercase
 digraph_index = {}
 letter_index = {l : 0 for l in allowed_letters}
 
-
 def process_text(text):
-    def process_word(word):
+    for word in text.lower().split():
         if len(word) > 1:
             digraphs = tuple(word[x:x + 2] for x in range(len(word) - 1))
-
             for dig in digraphs:
                 if dig[0] in allowed_letters and dig[1] in allowed_letters:
                     letter_index[dig[0]] += 1
                     digraph_index[dig] = digraph_index[dig] + 1 \
                             if dig in digraph_index else 1
-
-        # Words are guaranteed to be >0 char
+                            
         if word[-1] in allowed_letters:
             letter_index[word[-1]] += 1
-
-    for word in text.lower().split():
-        process_word(word)
-
 
 def process_data():
     total = sum(digraph_index.values())
 
-    # This calculates the PERCENTAGE over the COUNT
     for let in letter_index:
         letter_index[let] /= total
 
     for dig in digraph_index:
         digraph_index[dig] /= total
 
-    return sorted(digraph_index.items(), key=lambda o: o[1], reverse=True), \
-        sorted(letter_index.items(), key=lambda o: o[1], reverse=True)
-
+    return sorted(letter_index.items(), key=lambda o: o[1], reverse=True), \
+        sorted(digraph_index.items(), key=lambda o: o[1], reverse=True)[:10]
 
 if __name__ == '__main__':
     for arg in sys.argv[1:]:
